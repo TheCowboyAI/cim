0d90c428a8e1afa80f2cf3d9ea2d70fcc50a75d1
Steele Price <steele@thecowboy.ai>
2024-02-01 17:22:09 -0700

    redo

--- flake.nix
+++ flake.nix
@@ -35,6 +35,18 @@
               llm
               rust-analyzer
               vscode-extensions.vadimcn.vscode-lldb
+              cargo-edit
+              cargo-expand
+              cargo-udeps
+              cargo-whatfeatures
+              trunk
+              direnv
+              zsh
+              git
+              act
+              starship
+              tailwindcss
+              sass
             ];
 
           modules = [
@@ -42,61 +54,17 @@
             { programs.nix-ld.dev.enable = true; }
           ];
 
-          hardware.pulseaudio.enable = true;
-          hardware.pulseaudio.package = pulseaudioFull;
-          hardware.opengl.extraPackages = [ mesa.drivers ];
-
-          services.xserver.enable = true;
-
-          fonts.packages = with pkgs; [
-            noto-fonts
-            noto-fonts-cjk
-            noto-fonts-emoji
-            liberation_ttf
-            fira-code
-            fira-code-symbols
-            mplus-outline-fonts.githubRelease
-            dina-font
-            proggyfonts
-          ];
-
           devShells.default = mkShell {
             buildInputs = [
               rustToolchain
-              cargo-edit
-              cargo-expand
-              cargo-udeps
-              cargo-whatfeatures
               cargo-leptos
               cargo-generate
               cargo-make
-              docker
-              docker-compose
               cacert
-              trunk
-              direnv
-              lld
-              clang
-              gcc
-              zsh
-              git
-              act
-              musl
-              starship
               openssl
               openssl.dev
               pkg-config
               zlib.dev
-              alsa-lib
-              xorg.libX11
-              xorg.libXi
-              xorg.libXcursor
-              libpulseaudio
-              libGL
-              libglvnd
-              libiconv
-              tailwindcss
-              sass
               wasm-pack
               nodejs_18.out
               nodePackages.webpack
@@ -105,18 +73,7 @@
             ];
             
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
-            # see: https://discourse.nixos.org/t/running-a-rust-application-that-needs-egl-with-shell-nix/33245/3
-            LD_LIBRARY_PATH="${pkgs.libglvnd}/lib:/home/steele/git/cimlabs/drover/crates/ggml/sys/llama.cpp";
 
-          shellHook = ''
-            if [ -f .env ]; then
-              export $(grep -v '^#' .env | xargs)
-            fi
-            export GIT_CONFIG_NOSYSTEM=1
-            ZSH_CUSTOM=$HOME/.config/zsh
-            export PATH="$HOME/.cargo/bin:$PATH"
-            export LD_LIBRARY_PATH="${pkgs.libglvnd}/lib:/home/steele/git/cimlabs/drover/crates/ggml/sys/llama.cpp";
-            '';
         };
         }
       );
